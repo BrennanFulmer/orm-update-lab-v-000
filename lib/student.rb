4c9b11bbcb72dec6213d85d9a1ba3e782e6c644d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-02 17:51:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,5 +11,15 @@ class Student
     self
   end
 
-
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    
+    DB[:conn].execute(sql)
+end
 end
