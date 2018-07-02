381122faa561cb6d9e5900522bde1f99a1f4db64
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-02 17:59:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,9 +46,9 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = new(row[0])
+    new_student = new(row[1], row[0])
     new_student.id = 
-    new_student.name = row[1]
+    new_student.name = 
     new_student.grade = row[2]
     new_student
   end
